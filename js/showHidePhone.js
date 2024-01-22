43f67cb4de8340befce22d7905ade3d932ca5f80
Petko Todorov <p.todorov1015@gmail.com>
2024-01-22 10:46:00 +0200

    added copy button for phone number

--- js/showHidePhone.js
+++ js/showHidePhone.js
@@ -1,10 +1,24 @@
 function showHidePhoneNumber() {
-    let phoneNumberElement = document.getElementById('phoneNumber');
+    const phoneNumberElement = document.getElementById('phoneNumber');
     phoneNumberElement.addEventListener('click', () => {
         if (!phoneNumberElement.textContent.startsWith('+')) {
             phoneNumberElement.textContent = '+359 898 75 00 67';
+            copyButton.removeAttribute('hidden');
         }
     })
+
+    const copyButton = document.createElement('button');
+    copyButton.textContent = 'Copy';
+    copyButton.setAttribute('hidden', 'true');
+    copyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(phoneNumberElement.textContent);
+        copyButton.textContent = 'Copied!';
+        copyButton.disabled = true;
+    })
+
+    phoneNumberElement.after(copyButton);
+
 }
+
 
 showHidePhoneNumber()
