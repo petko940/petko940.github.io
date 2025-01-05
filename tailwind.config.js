bee4663653c07d10bbd0c6013f0593faedb587ad
Petko Todorov <p.todorov1015@gmail.com>
2025-01-05 20:56:57 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Open Sans', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
