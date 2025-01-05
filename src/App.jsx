c8de4ddc976c892698a728ecb035fb1ba9039f2f
Petko Todorov <p.todorov1015@gmail.com>
2025-01-05 20:56:29 +0200

    add intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Intro from './components/intro'
 
 function App() {
 
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <Intro />
         </>
     )
 }
