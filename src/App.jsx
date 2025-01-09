43fb377c9eb72bf00aef15b56ee2826c58fd50f2
Petko Todorov <p.todorov1015@gmail.com>
2025-01-09 11:16:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import { useEffect } from 'react'
+import AOS from 'aos';
 import Home from './components/Home'
 import NavBar from './components/NavBar'
 import AboutMe from './components/AboutMe'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Certificates from './components/Certificates'
+import Contact from './components/Contact'
+
 
 function App() {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+        });
+    }, []);
 
     return (
         <>
@@ -16,8 +27,9 @@ function App() {
             <Skills />
             <Projects />
             <Certificates />
+            <Contact />
         </>
     )
 }
 
-export default App
+export default App;
