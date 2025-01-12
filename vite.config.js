f38bcba370ae6613ac3b237f43b38e052fbd3d5b
Petko Todorov <p.todorov1015@gmail.com>
2025-01-12 20:29:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: '0.0.0.0', 
-        port: 3456,      
-    },
+    base: '/petko940.github.io/',
 })
